3bc997d6d856bec48bce17a59ab768fa6caa42f3
DelsemmeJolien <r0834369@student.thomasmore.be>
2026-04-30 09:55:06 +0200

    overige testbestanden verwijderen (win-lose)

--- lose.js
+++
@@ -1,6 +0,0 @@
-const livesLeft = localStorage.getItem("livesLeft");
-const lettersGuessed = localStorage.getItem("lettersGuessed");
-
-document.querySelector(".lives-left").innerHTML = livesLeft;
-document.querySelector(".guessed-letters").innerHTML = lettersGuessed;
-document.querySelector(".ranking").innerHTML = "Last";
